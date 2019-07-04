e23137d4cec64dca7edb036d086660949055dcfe
kxdang <ikien.dang@gmail.com>
2019-07-04 19:15:37 -0400

    fixing desktop

--- gatsby-node.js
+++ gatsby-node.js
@@ -51,7 +51,7 @@ exports.createPages = ({ graphql, actions }) => {
     })
 
     // Create blog post list pages
-    const postsPerPage = 2
+    const postsPerPage = 5
     const numPages = Math.ceil(posts.length / postsPerPage)
 
     Array.from({ length: numPages }).forEach((_, i) => {
