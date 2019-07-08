ba830f9763b7ced5ff5bc3e64a7f53245fd6a80d
kxdang <ikien.dang@gmail.com>
2019-07-08 16:31:12 -0400

    fixing darkmode issue with vertical height using min-height

--- src/components/layout.js
+++ src/components/layout.js
@@ -97,6 +97,7 @@ class Layout extends React.Component {
                 marginRight: `auto`,
                 maxWidth: rhythm(24),
                 padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+                minHeight: `100vh`,
               }}
             >
               <header>{header}</header>
