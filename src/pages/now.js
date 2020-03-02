49b385486545c61990ab3a05fa7da3548db5ed5b
kxdang <ikien.dang@gmail.com>
2020-03-01 19:10:16 -0500

    grammar

--- src/pages/now.js
+++ src/pages/now.js
@@ -87,7 +87,7 @@ class now extends React.Component {
               <p>I still play CS:GO during my leisure time<span role="img" aria-label="SunglassSmiley">😎</span>. I have been bouncing between Vermintide 2 with a couple of my friends and playing Borderlands 3!</p>
 
               <h3 style={{ textAlign: "center" }}>Side-Projects</h3>
-              <p>I am helping my friend with his application for CS:GO called <a href="https://scrim.app/" target="_blank" rel="noopener noreferrer">Scrim.app</a> and building multiple projects with React found in my <a href="https://github.com/kxdang" target="_blank" rel="noopener noreferrer">Github.</a></p>
+              <p>I am helping my friend with his service for CS:GO called <a href="https://scrim.app/" target="_blank" rel="noopener noreferrer">Scrim.app</a> and building multiple projects with React found in my <a href="https://github.com/kxdang" target="_blank" rel="noopener noreferrer">Github.</a></p>
 
               <p style={{ textAlign: "center", marginBottom: "0", fontSize: "0.8em", fontStyle: "italic" }}>Last updated (March 1st 2020)</p>
             </div>
