ca5916716a635ca653b7d13d355c5ea640156a0b
kxdang <ikien.dang@gmail.com>
2020-04-08 17:11:47 -0400

    removing image background issue with gatsby

--- src/components/bar.js
+++ src/components/bar.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import { Bar } from 'react-chartjs-2';
 
 const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
     datasets: [
         {
-            label: 'My First dataset',
+            label: 'Pomodoros',
             backgroundColor: 'rgba(255,99,132,0.2)',
             borderColor: 'rgba(255,99,132,1)',
             borderWidth: 1,
             hoverBackgroundColor: 'rgba(255,99,132,0.4)',
             hoverBorderColor: 'rgba(255,99,132,1)',
-            data: [65, 59, 80, 81, 56, 55, 40]
+            data: [65, 59, 80, 81, 56, 55, 40, 46, 50, 12, 60, 12]
         }
     ]
 };
