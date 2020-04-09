258978011be929adfc29913f586f8969acbf7750
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-04-09 11:00:15 -0400

    editing piechart colours, swapping green and blue for contrast with purple

--- content/blog/075-mdx/pie.js
+++ content/blog/075-mdx/pie.js
@@ -1,61 +1,48 @@
-import React, { Component } from 'react';
-import { Pie } from 'react-chartjs-2';
+import React, { Component } from "react"
+import { Pie } from "react-chartjs-2"
 
 const data = {
-    labels: [
-        'Luck',
-        'Skill',
-        'Will',
-        'Pleasure',
-        'Pain'
-    ],
-    datasets: [{
-        data: [10, 20, 15, 5, 50],
-        backgroundColor: [
-            '#f4f4f4',
-            '#8ec6c5',
-            '#6983aa',
-            '#8566aa',
-            '#d7385e'
-        ],
-        hoverBackgroundColor: [
-            '#FFF',
-            '#b8f2f1',
-            '#8b9db9',
-            '#aa9cbc',
-            '#db7d93'
-        ]
-    }],
+  labels: ["Luck", "Skill", "Concentrated Power of Will", "Pleasure", "Pain"],
+  datasets: [
+    {
+      data: [10, 20, 15, 5, 50],
+      backgroundColor: ["#f4f4f4", "#6983aa", "#8ec6c5", "#8566aa", "#d7385e"],
+      hoverBackgroundColor: [
+        "#FFF",
+        "#8b9db9",
+        "#b8f2f1",
+        "#aa9cbc",
+        "#db7d93",
+      ],
+    },
+  ],
 }
 
 const option = {
-    tooltips: {
-        callbacks: {
-            label: function (tooltipItem, data) {
-                var dataset = data.datasets[tooltipItem.datasetIndex];
-                var meta = dataset._meta[Object.keys(dataset._meta)[0]];
-                var total = meta.total;
-                var currentValue = dataset.data[tooltipItem.index];
-                var percentage = parseFloat((currentValue / total * 100).toFixed(1));
-                return percentage + '%';
-            },
-            title: function (tooltipItem, data) {
-                return data.labels[tooltipItem[0].index];
-            }
-        }
-    }
+  tooltips: {
+    callbacks: {
+      label: function(tooltipItem, data) {
+        var dataset = data.datasets[tooltipItem.datasetIndex]
+        var meta = dataset._meta[Object.keys(dataset._meta)[0]]
+        var total = meta.total
+        var currentValue = dataset.data[tooltipItem.index]
+        var percentage = parseFloat(((currentValue / total) * 100).toFixed(1))
+        return percentage + "%"
+      },
+      title: function(tooltipItem, data) {
+        return data.labels[tooltipItem[0].index]
+      },
+    },
+  },
 }
 
-
 export default class PieChart extends Component {
-
-    render() {
-
-        return (
-            <div>
-                <h2 style={{ textAlign: "center" }}>Kien Dang</h2>
-                <Pie data={data} options={option} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2 style={{ textAlign: "center" }}>Kien Dang</h2>
+        <Pie data={data} options={option} />
+      </div>
+    )
+  }
 }
